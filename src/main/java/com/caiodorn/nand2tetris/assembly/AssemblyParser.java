c01577e134fd26cfa062630b6625b66a7a0cc91e
caiodorn <caiodorn@gmail.com>
2020-07-05 17:22:46 +0100

    Base assembler structure, static instruction tables.

---
+++ src/main/java/com/caiodorn/nand2tetris/assembly/AssemblyParser.java
@@ -0,0 +1,20 @@
+package com.caiodorn.nand2tetris.assembly;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AssemblyParser {
+
+    private static final Map<String, String> predefinedSymbols = new HashMap<>();
+
+    static {
+        //TODO
+        predefinedSymbols.put("", "one");
+        predefinedSymbols.put("", "two");
+    }
+
+    public void parse(List<String> asmCommands) {
+        //TODO
+    }
+}
---
+++ src/main/java/com/caiodorn/nand2tetris/assembly/Instructions.java
@@ -0,0 +1,70 @@
+package com.caiodorn.nand2tetris.assembly;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Instructions {
+
+    public static final Map<String, String> comp;
+    public static final Map<String, String> dest;
+    public static final Map<String, String> jump;
+
+    static {
+        Map<String, String> c = new HashMap<>();
+        c.put("0",   "101010");
+        c.put("1",   "111111");
+        c.put("-1",  "111010");
+        c.put("D",   "001100");
+        c.put("A",   "110000");
+        c.put("M",   "110000");
+        c.put("!D",  "001101");
+        c.put("!A",  "110001");
+        c.put("!M",  "110001");
+        c.put("-D",  "001111");
+        c.put("-A",  "110011");
+        c.put("-M",  "110011");
+        c.put("D+1", "011111");
+        c.put("A+1", "110111");
+        c.put("M+1", "110111");
+        c.put("D-1", "001110");
+        c.put("A-1", "110010");
+        c.put("M-1", "110010");
+        c.put("D+A", "000010");
+        c.put("D+M", "000010");
+        c.put("D-A", "010011");
+        c.put("D-M", "010011");
+        c.put("A-D", "000111");
+        c.put("M-D", "000111");
+        c.put("D&A", "000000");
+        c.put("D&M", "000000");
+        c.put("D|A", "010101");
+        c.put("D|M", "010101");
+        comp = Collections.unmodifiableMap(c);
+
+        Map<String, String> d = new HashMap<>();
+        d.put("null", "000");
+        d.put("M",    "001");
+        d.put("D",    "010");
+        d.put("MD",   "011");
+        d.put("A",    "100");
+        d.put("AM",   "101");
+        d.put("AD",   "110");
+        d.put("AMD",  "111");
+        dest = Collections.unmodifiableMap(d);
+
+        Map<String, String> j = new HashMap<>();
+        j.put("null", "000");
+        j.put("JGT",  "001");
+        j.put("JEQ",  "010");
+        j.put("JGE",  "011");
+        j.put("JLT",  "100");
+        j.put("JNE",  "101");
+        j.put("JLE",  "110");
+        j.put("JMP",  "111");
+        jump = Collections.unmodifiableMap(j);
+    }
+
+    private Instructions() {}
+
+}
